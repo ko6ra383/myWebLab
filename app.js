180377ec6a67483c501532882d32237909dfc911
Ko6ra <ko6ra383@mail.ru>
2023-02-10 03:00:18 +0700

    Кнопки удаления и скрытие элементов

--- app.js
+++ app.js
@@ -8,4 +8,30 @@ function addTask(){
     document.querySelector('.list').appendChild(li)
     document.querySelector('#inp').value = ''
 
+    let span = document.createElement('span')
+    let char = document.createTextNode('\u2715')
+    span.appendChild(char)
+    span.className = 'closeBtn'
+    li.appendChild(span)
+    span.onclick = () => {
+        let parent = span.parentElement
+        parent.style.display = 'none'
+    }
 }
+
+function addCloseButtons(){
+    let list = document.querySelectorAll('li')
+    for (const el of list) {
+        let span = document.createElement('span')
+        let char = document.createTextNode('\u2715')
+        span.appendChild(char)
+        span.className = 'closeBtn'
+        el.appendChild(span)
+        span.onclick = () => {
+            let parent = span.parentElement
+            parent.style.display = 'none'
+        }
+    }
+}
+
+addCloseButtons()
